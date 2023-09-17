aaea92a65a5984aeab507ec6c162301ac463cb4b
Шубин Григорий <grisha.shubin@gmail.com>
2023-09-18 03:18:13 +0600

    fix users

--- backend/users/models.py
+++ backend/users/models.py
@@ -24,11 +24,13 @@ class Subscription(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='follower',
+        verbose_name='Подписчик',
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='following',
+        verbose_name='Автор подписки',
     )
 
     class Meta:
@@ -41,6 +43,8 @@ class Subscription(models.Model):
                 name='check_author'
             )
         ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
 
     def __str__(self):
         return f'{self.user.username} - {self.author.username}'
